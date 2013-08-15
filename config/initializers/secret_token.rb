49a17aceffa9ed1ed34b2398b16dde54c06b24f8
Phil <phil@crowdtilt.com>
2013-08-14 17:39:22 -0700

    first commit dawg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '70758414ab6ba51094856ff3b5b45334932ca7bb2f1897eee62de0ee84dcdcc14ac369a25c40b87733301f92fff51135f4b5d7546cca98024aea8bc58dac47dd'
